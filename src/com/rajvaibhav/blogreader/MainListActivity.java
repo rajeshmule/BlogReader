cd7aa1c63d0e61b8f1f24ba31a59d9e750cd3af5
YOUR NAME <rajeshsmule@gmail.com>
2014-11-26 15:18:10 +0530

    complete project

--- src/com/rajvaibhav/blogreader/MainListActivity.java
+++ src/com/rajvaibhav/blogreader/MainListActivity.java
@@ -7,19 +7,30 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
@@ -27,15 +38,21 @@ public class MainListActivity extends ListActivity {
 //	protected String[] mAndroidName;
 	protected String[] mBlogPostTitles;
 	public static final int NUMBER_OF_POSTS = 20;
-	public static final String TAG = MainListActivity.class.getSimpleName();
+	public static final String TAG  = MainListActivity.class.getSimpleName();
 	protected JSONObject mBlogData;
+	protected ProgressBar mProgressBar;
+	
+	
+	private final String KEY_TITLE = "title"; 
+	private final String KEY_AUTHOR = "author";
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 		
-		
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
 	/*	Resources resources = getResources();
 		mAndroidName = resources.getStringArray(R.array.android_name); 
 		
@@ -45,6 +62,7 @@ public class MainListActivity extends ListActivity {
 		//Toast.makeText(this,  mas, Toast.LENGTH_SHORT).show();*/
 	
 		if(isNetworkAvailable()){
+			mProgressBar.setVisibility(View.VISIBLE);
 			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
 			getBlogPostsTask.execute();
 			
@@ -55,6 +73,29 @@ public class MainListActivity extends ListActivity {
 		
  	}
 
+	@Override
+		protected void onListItemClick(ListView l, View v, int position, long id) {
+		
+			super.onListItemClick(l, v, position, id);
+			try {
+	    		JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+	        	JSONObject jsonPost = jsonPosts.getJSONObject(position);
+	        	
+				String blogUrl = jsonPost.getString("url");
+				
+				Intent intent = new Intent(this, BlogViewActivity.class);
+				intent.setData(Uri.parse(blogUrl));
+				startActivity(intent);
+				
+			} catch (JSONException e) {
+				logException(e);
+			}
+		}
+
+	private void logException(Exception e) {
+		Log.e(TAG,"Exception caught!",e);
+	}
+	
 	private boolean isNetworkAvailable() {
 		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
@@ -65,26 +106,57 @@ public class MainListActivity extends ListActivity {
 		return isAvailable;
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_list, menu);
-		return true;
-	}
 	
-	public void updateList() {
+	public void handleBlogResponse() {
+		mProgressBar.setVisibility(View.INVISIBLE);
 		if (mBlogData == null){
-			
+			updeteDisplayForError();
 		}
 		else{
 			try {
-				Log.d(TAG, mBlogData.toString(2));
+				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+				ArrayList<HashMap<String, String>> blogPosts = new ArrayList<HashMap<String, String>>();
+	        	//mBlogPostTitles = new String[jsonPosts.length()];
+	        	for(int i = 0; i < jsonPosts.length(); i++ ){
+	        		JSONObject post = jsonPosts.getJSONObject(i);
+	        		String title = post.getString(KEY_TITLE);
+	        		title = Html.fromHtml(title).toString(); 
+	        		//mBlogPostTitles[i]=title;
+	        		String author = post.getString(KEY_AUTHOR);
+	        		author = Html.fromHtml(author).toString();
+	        	
+	        		HashMap<String, String> blogPost = new HashMap<String, String>();
+	        		blogPost.put(KEY_TITLE, title);
+	        		blogPost.put(KEY_AUTHOR, author);
+	        		
+	        		blogPosts.add(blogPost);  
+	        	}
+	        	
+				String[] keys = {KEY_TITLE,KEY_AUTHOR};
+				int[] ids = {android.R.id.text1,android.R.id.text2};
+				SimpleAdapter adapter = new SimpleAdapter(this,blogPosts,android.R.layout.simple_list_item_2,keys,ids);
+	        	//ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+	        	setListAdapter(adapter);
+ 
+	        	
 			} catch (JSONException e) {
 				Log.d(TAG, "Expetion caugch!", e);
 			}
 		}
 		
+	}
+
+	private void updeteDisplayForError() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(getString(R.string.error_title));
+		builder.setMessage(getString(R.string.error_massage));
+		builder.setPositiveButton(android.R.string.ok, null);
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+		TextView emptyTextView = (TextView) getListView().getEmptyView();
+		emptyTextView.setText(getString(R.string.no_item));
 	}
 
 	@Override
@@ -137,20 +209,20 @@ public class MainListActivity extends ListActivity {
 				
 			}
 			catch(MalformedURLException e){ 
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			}
 			catch(IOException e){
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			}
 			catch(Exception e){
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			}
 			return jsonResponse;
 		}
 		@Override
 		protected void onPostExecute(JSONObject result){
 			mBlogData = result;
-			updateList();
+			handleBlogResponse();
 		}
 	}
 
